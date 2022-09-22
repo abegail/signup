7075c5e46551afea08d5be01eaadb91a9bd4e0d0
Abegail Pates <abegail.pates@gmail.com>
2022-09-22 17:11:57 +0800

    Add styling to form

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function verifyMatchingPasswords() {
-    const pw1 = document.getElementById('password').value;
-    const pw2 = document.getElementById('confirm-password').value;
-    if (pw1 !== pw2) {
+    const pw1 = document.getElementById('password');
+    const pw2 = document.getElementById('confirm-password');
+    if (pw1.value !== pw2.value) {
         document.getElementById('password-check').textContent = "* Passwords do not match."
+        pw1.classList.add('invalid');
+        pw2.classList.add('invalid');
         return false;
     } else {
         return true;
